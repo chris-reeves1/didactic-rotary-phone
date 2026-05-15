79ab8cb6ac1121cc300d5a228e1d8ff8d250327f
chris-reeves1 <91483522+chris-reeves1@users.noreply.github.com>
2026-05-15 09:53:41 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -11,7 +11,7 @@ class FlaskAppTests(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
     
     def test_health_route(self):
-        resposne = self.client.get("/health")
+        response = self.client.get("/health")
         self.assertEqual(response.status_code, 200)
     
 if __name__ == "__main__":
